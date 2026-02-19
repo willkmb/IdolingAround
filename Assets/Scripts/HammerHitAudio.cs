744e82a081d589c9fa88943697fb981d02f422e9
SeddonEJ <emilyseddon@hotmail.co.uk>
2026-02-19 13:39:23 +0000

    fuckin pots

--- Assets/Scripts/HammerHitAudio.cs
+++ Assets/Scripts/HammerHitAudio.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class HammerHitAudio : MonoBehaviour
 {
     AudioSource sound;
+    [SerializeField] Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +18,9 @@ public class HammerHitAudio : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(rb.linearVelocity.magnitude);
+        sound.volume = 0.5f * rb.linearVelocity.magnitude/8;
+        sound.pitch = Random.Range(0.9f, 1.1f);
         sound.Play();
     }
 }
