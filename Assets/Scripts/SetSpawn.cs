d14c0705cdcb4e784ef8813160874ffbf4fff44e
SeddonEJ <emilyseddon@hotmail.co.uk>
2026-02-20 17:52:24 +0000

    particles respawn

--- Assets/Scripts/SetSpawn.cs
+++ Assets/Scripts/SetSpawn.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class SetSpawn : MonoBehaviour
 {
     MovementScript movementScript;
+    AudioSource sound;
+    [SerializeField] ParticleSystem particle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movementScript = GameObject.FindFirstObjectByType<MovementScript>().GetComponent<MovementScript>();
+        sound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,6 +18,14 @@ public class SetSpawn : MonoBehaviour
         if (other.gameObject.layer == 6)
         {
             movementScript.respawnPoint = this.gameObject.transform;
+            sound.Play();
+            particle.Play();
+            //Invoke("ParticleOff", 1f);
         }
+    }
+
+    void ParticleOff()
+    {
+        //particle.SetActive(false);
     }
 }
