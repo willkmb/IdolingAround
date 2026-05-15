d728b0cf9a454eeca5fccef4aac1b93813c050bd
willkmb <thewillkimber@gmail.com>
2026-05-15 17:06:31 +0100

    updates

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -365,7 +365,7 @@ public class MovementScript : MonoBehaviour
         Debug.DrawRay(cube.transform.position, cube.transform.forward * 3f, Color.blue);
         Debug.DrawRay(cube.transform.position, velDir * 3f, Color.red);
 
-        //if (alignDot < 0f && Input.GetAxis("Vertical") > 0f) cube.transform.Rotate(Vector3.up, 180f, Space.World);
+        if (alignDot < 0f && Input.GetAxis("Vertical") > 0f && speed > 2f) cube.transform.Rotate(Vector3.up, 180f, Space.World);
     }
 
     private void HandleChargeDrain()
