0028854b6f5d88c8dc7b3ff2a798b105c21d138a
willkmb <thewillkimber@gmail.com>
2026-06-08 19:56:55 +0100

    beginning menu functionality

--- Assets/Scripts/ButtonHover.cs
+++ Assets/Scripts/ButtonHover.cs
@@ -19,6 +19,7 @@ public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
                 anim.Blend(state.name);
             }
         }
+        GetComponent<AudioSource>().Play();
     }
 
     public void OnPointerExit(PointerEventData eventData)
--- Assets/Scripts/musicManager.cs
+++ Assets/Scripts/musicManager.cs
@@ -4,6 +4,6 @@ public class musicManager : MonoBehaviour
 {
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(this);
     }
 }
