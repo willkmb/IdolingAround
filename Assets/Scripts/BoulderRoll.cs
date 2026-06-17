744d740ba641d158b44b82db589ca94be710e125
SeddonEJ <emilyseddon@hotmail.co.uk>
2026-06-17 01:29:53 +0100

    bouldersound

--- Assets/Scripts/BoulderRoll.cs
+++ Assets/Scripts/BoulderRoll.cs
@@ -7,12 +7,13 @@ public class BoulderRoll : MonoBehaviour
 {
     [SerializeField] VoiceLinesBoulderChase chaseVoicelines;
     [SerializeField] AudioSource voiceSource;
+    
     [SerializeField] AudioClip audioClip;
     [HideInInspector] public Transform boulderRespawn;
     [HideInInspector] public float boulderRespawnTime;
     SplineAnimate splineAnim;
     Animation rollAnim;
-
+    AudioSource rollSource;
     Rigidbody rb;
 
     float targetDistance;
@@ -24,6 +25,7 @@ public class BoulderRoll : MonoBehaviour
         splineAnim = GetComponent<SplineAnimate>();
         rb = GetComponent<Rigidbody>();
         rollAnim = GetComponentInChildren<Animation>();
+        rollSource = GetComponent<AudioSource>();
     }
 
     private void FixedUpdate()
@@ -36,6 +38,8 @@ public class BoulderRoll : MonoBehaviour
                 voiceSource.clip = audioClip;
                 voiceSource.Play();
                 rollAnim.Stop();
+                rollSource.loop = false;
+                rollSource.Stop();
                 isEndOfCorridor = true;
             }
         }
