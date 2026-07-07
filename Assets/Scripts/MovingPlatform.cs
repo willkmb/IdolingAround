f28863948d6a72a729202c1ef81a1895ff2aea87
SeddonEJ <emilyseddon@hotmail.co.uk>
2026-07-07 15:53:41 +0100

    moving and sinking platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -13,6 +13,7 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] float snapDist = 0.01f;
 
     Vector3 targetPos;
+    Vector3 midPos;
     Rigidbody rb;
     float totalDist;
     float timer;
@@ -21,6 +22,10 @@ public class MovingPlatform : MonoBehaviour
     {
         transform.position = pointA.transform.position;
         targetPos = pointB.transform.position;
+        var midPosX = (pointA.transform.position.x + pointB.transform.position.x) / 2;
+        var midPosY = (pointA.transform.position.y + pointB.transform.position.y) / 2;
+        var midPosZ = (pointA.transform.position.z + pointB.transform.position.z) / 2;
+        midPos = new Vector3(midPosX, midPosY, midPosZ);
         totalDist = Vector3.Distance(pointA.transform.position, pointB.transform.position);
         rb = GetComponent<Rigidbody>();
 
@@ -37,7 +42,9 @@ public class MovingPlatform : MonoBehaviour
         
         if ((targetPos - transform.position).sqrMagnitude > snapDist)
         {
-            Vector3 newPos = Vector3.MoveTowards(rb.position, targetPos, speed * Time.fixedDeltaTime);
+            float dist = Vector3.Distance(transform.position, midPos);
+            float distPerc = Mathf.Clamp(1 - (dist / (totalDist / 2)), 0.5f, 1f);
+            Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime * distPerc);
             rb.MovePosition(newPos);
         }
         else
@@ -51,22 +58,6 @@ public class MovingPlatform : MonoBehaviour
                 targetPos = targetPos == pointA.transform.position ? pointB.transform.position : pointA.transform.position;
                 timer = 0f;
             }
-        }
-        
-    }
-
-    IEnumerator MovePlatform()
-    {
-        while (true)
-        {
-            while ((targetPos - transform.position).sqrMagnitude > snapDist)
-            {
-                Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-                rb.MovePosition(newPos);
-                yield return null;
-            }
-            targetPos = targetPos == pointA.transform.position ? pointB.transform.position : pointA.transform.position;
-            yield return new WaitForSeconds(waitTime);
-        }
+        }   
     }
 }
