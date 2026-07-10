67ad72ffd9fe1b2d2b0b96132556f28290bc4c79
SeddonEJ <emilyseddon@hotmail.co.uk>
2026-07-10 15:21:42 +0100

    crushing walls

--- Assets/Scripts/CrushingWalls.cs
+++ Assets/Scripts/CrushingWalls.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class CrushingWalls : MonoBehaviour
 {
     RespawnPlayer respawnScript;
-    [SerializeField] float timeToClose;
+    [SerializeField] float speed;
+
+    [Header("Set In Prefab, Do Not Change")]
     [SerializeField] GameObject[] walls;
     [SerializeField] GameObject midPoint1;
     [SerializeField] GameObject midPoint2;
@@ -86,13 +88,13 @@ public class CrushingWalls : MonoBehaviour
     {
         if (Vector3.Distance(walls[0].transform.localPosition, midPoint1.transform.localPosition) > 0.05f)
         {
-            walls[0].transform.localPosition = Vector3.MoveTowards(walls[0].transform.localPosition, midPoint1.transform.localPosition, timeToClose * Time.deltaTime);
+            walls[0].transform.localPosition = Vector3.MoveTowards(walls[0].transform.localPosition, midPoint1.transform.localPosition, speed * Time.deltaTime);
         }
         else { walls[0].transform.localPosition = midPoint1.transform.localPosition; }
 
         if (Vector3.Distance(walls[1].transform.localPosition, midPoint2.transform.localPosition) > 0.05f)
         {
-            walls[1].transform.localPosition = Vector3.MoveTowards(walls[1].transform.localPosition, midPoint2.transform.localPosition, timeToClose * Time.deltaTime);
+            walls[1].transform.localPosition = Vector3.MoveTowards(walls[1].transform.localPosition, midPoint2.transform.localPosition, speed * Time.deltaTime);
         }
         else { walls[1].transform.localPosition = midPoint2.transform.localPosition; }
 
@@ -102,15 +104,15 @@ public class CrushingWalls : MonoBehaviour
     {
         if (Vector3.Distance(walls[0].transform.localPosition, Vector3.zero) > 0.05f)
         {
-            walls[0].transform.localPosition = Vector3.MoveTowards(walls[0].transform.localPosition, midPoint2.transform.localPosition, timeToClose * Time.deltaTime);
+            walls[0].transform.localPosition = Vector3.MoveTowards(walls[0].transform.localPosition, Vector3.zero, speed * Time.deltaTime);
         }
-        else { walls[0].transform.localPosition = midPoint1.transform.localPosition; }
+        else { walls[0].transform.localPosition = Vector3.zero; }
 
         if (Vector3.Distance(walls[1].transform.localPosition, midPoint2.transform.localPosition) > 0.05f)
         {
-            walls[1].transform.localPosition = Vector3.MoveTowards(walls[1].transform.localPosition, midPoint2.transform.localPosition, timeToClose * Time.deltaTime);
+            walls[1].transform.localPosition = Vector3.MoveTowards(walls[1].transform.localPosition, Vector3.zero, speed * Time.deltaTime);
         }
-        else { walls[1].transform.localPosition = midPoint2.transform.localPosition; }
+        else { walls[1].transform.localPosition = Vector3.zero; }
     }
 
 }
